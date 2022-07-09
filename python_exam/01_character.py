3b66e9d399acaab1aef68b27942e3135671de7c3
jagjamin <jamjamssert@gmail.com>
2022-07-09 12:25:43 +0900

    7월9일 시험

---
+++ python_exam/01_character.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from dataclasses import dataclass
+import random
+
+character = data
+
+running = True
+ 
+while running
+ data = input(용사,도적,검사,마법사,도사,주술사,드루이드,변신술사,사제,암살자)
